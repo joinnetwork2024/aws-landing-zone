dac057c892582a3600e587d34803a5cd4f8ba104
joinnetwork2024 <joejo201770@gmail.com>
2026-01-25 22:58:29 +0000

    update workflow 3rd

--- environments/dev/flashsale/variables_def.tf
+++ environments/dev/flashsale/variables_def.tf
@@ -3,3 +3,16 @@ variable "db_password" {
   type        = string
   default     = "TestingSuperStrongPass"
 }
+
+variable "aws_region" {
+  description = "AWS region"
+  default     = "eu-west-2"
+  type        = string
+}
+
+
+variable "project_name" {
+  description = "Project name"
+  default     = "landing"
+  type        = string
+}
--- environments/dev/iotmonitor/variables_def.tf
+++ environments/dev/iotmonitor/variables_def.tf
@@ -1,16 +1,9 @@
-variable "env" {
-  default = "dev"
-}
-
-variable "cloud_provider" {
-  default = "aws" # or "azure"
-}
-
 variable "aws_region" {
   description = "AWS region"
   default     = "eu-west-2"
   type        = string
 }
+
 
 variable "project_name" {
   description = "Project name"
